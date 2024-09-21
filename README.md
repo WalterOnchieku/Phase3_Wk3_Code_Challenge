efdbe01cf7310bdb6ac4181e95f43f5b221d7304
root <root@DESKTOP-RHA5CJR.localdomain>
2024-09-21 20:38:18 +0300

    final

--- README.md
+++ README.md
@@ -1,1 +1,83 @@
-WK3 CODE CHALLENGE
+# WK3 CODE CHALLENGE - CONCERTS
+
+## Overview
+The goal of this project is to build a **Concert Management Domain** using **Python 3** and **SQLAlchemy**. This challenge will test the following topics:
+- SQLAlchemy Migrations
+- SQLAlchemy Relationships
+- Class and Instance Methods
+- SQLAlchemy Querying
+
+## Project Structure
+The main files for this project are:
+- `models.py`
+- `app.py`
+
+### 1. `models.py`
+This file contains the models responsible for creating the tables in the database. There are three models:
+
+- **Band**: `name`, `hometown`
+- **Venue**: `title`, `city`
+- **Concert**
+
+The **Concert** table serves as an association table and contains two foreign keys: one matching the `Band` table's `id`, and the other matching the `Venue` table's `id`.
+
+### Relationships
+The following relationships between the tables have been defined:
+- A Band has many Concerts.
+- A Venue has many Concerts.
+- A Concert belongs to both a Band and a Venue.
+- The relationship between Band and Venue is many-to-many.
+
+### Methods
+Each model (class) has the following methods defined:
+
+#### Object Relationship Methods
+**Concert**
+- `band()`: Returns the Band instance for this Concert.
+- `venue()`: Returns the Venue instance for this Concert.
+  
+(Note: The above methods were implemented directly from the relationships and do not need to be defined separately as they would be redundant.)
+
+**Venue**
+- `concerts()`: Returns a collection of all the concerts for the Venue.
+- `bands()`: Returns a collection of all the bands who performed at the Venue.
+
+**Band**
+- `concerts()`: Returns a collection of all the concerts that the Band has played.
+- `venues()`: Returns a collection of all the venues where the Band has performed.
+
+#### Aggregate and Relationship Methods
+**Concert**
+- `hometown_show()`: Returns `True` if the concert is in the band's hometown, otherwise `False`.
+- `introduction()`: Returns a string in the form:  
+  `"Hello {venue city}!!!!! We are {band name} and we're from {band hometown}"`.
+
+**Band**
+- `play_in_venue(venue, date)`: Takes a `Venue` instance and `date` (as a string) and creates a new concert for the band at that venue on that date.
+- `all_introductions()`: Returns an array of strings representing all the introductions for this band in the form:  
+  `"Hello {venue city}!!!!! We are {band name} and we're from {band hometown}"`.
+- `most_performances()` (class method): Returns the Band instance that has played the most concerts.
+
+**Venue**
+- `concert_on(date)`: Takes a `date` (string) and returns the first concert on that date at the venue.
+- `most_frequent_band()`: Returns the band with the most concerts at the venue.
+
+### 2. `app.py`
+This file contains:
+- Instances of Band, Venue, and Concert to populate the corresponding tables.
+- Implementations of all the methods defined in `models.py`.
+- Additional queries to manage and manipulate the concert data.
+
+## Installation
+Clone the repository:
+    ```bash
+    git clone <https://github.com/WalterOnchieku/Phase3_Wk3_Code_Challenge>
+    ```
+
+
+## Usage
+Once cloned, type code . to open in VS code
+To run the application, execute:
+```bash
+python app.py
+NB: ensure you are in the project root directory
--- app.py
+++ app.py
@@ -7,125 +7,125 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 #band instances
-# band1 = Band(name="Sauti Sol", hometown="Kakamega")
-# band2 = Band(name="Elani", hometown="Nairobi")
-# band3 = Band(name="Jabali", hometown="Mombasa")
+band1 = Band(name="Sauti Sol", hometown="Kakamega")
+band2 = Band(name="Elani", hometown="Nairobi")
+band3 = Band(name="Jabali", hometown="Mombasa")
 
-# #venue instances
-# venue1 = Venue(title="Safari Park Hotel", city="Nairobi")
-# venue2 = Venue(title="Sarova Whitesands Beach Resort", city="Mombasa")
-# venue3 = Venue(title="Leisure Lodge Hotel", city="Diani")
+#venue instances
+venue1 = Venue(title="Safari Park Hotel", city="Nairobi")
+venue2 = Venue(title="Sarova Whitesands Beach Resort", city="Mombasa")
+venue3 = Venue(title="Leisure Lodge Hotel", city="Diani")
 
-# #concert instances
-# concert1 = Concert(date="2024/09/11", band=band2, venue=venue1)
-# concert2 = Concert(date="2024/12/08", band=band3, venue=venue2)
-# concert3 = Concert(date="2024/10/15", band=band1, venue=venue3)
-# concert4 = Concert(date="2024/09/19", band=band2, venue=venue3)
-# concert5 = Concert(date="2024/11/15", band=band3, venue=venue2)
-# concert6 = Concert(date="2024/10/12", band=band1, venue=venue1)
+#concert instances
+concert1 = Concert(date="2024/09/11", band=band2, venue=venue1)
+concert2 = Concert(date="2024/12/08", band=band3, venue=venue2)
+concert3 = Concert(date="2024/10/15", band=band1, venue=venue3)
+concert4 = Concert(date="2024/09/19", band=band2, venue=venue3)
+concert5 = Concert(date="2024/11/15", band=band3, venue=venue2)
+concert6 = Concert(date="2024/10/12", band=band1, venue=venue1)
 
-# #add all instances to session
-# session.add_all([band1, band2, band3, venue1, venue2, venue3, concert1, concert2, concert3, concert4, concert5, concert6])
-# #commit transaction to save to database
-# session.commit()
+#add all instances to session
+session.add_all([band1, band2, band3, venue1, venue2, venue3, concert1, concert2, concert3, concert4, concert5, concert6])
+#commit transaction to save to database
+session.commit()
 
-# concerts = session.query(Concert).all()
-# for concert in concerts:
-#     print(f"Band: {concert.band.name} | Venue: {concert.venue.title} | Date: {concert.date}")
+concerts = session.query(Concert).all()
+for concert in concerts:
+    print(f"Band: {concert.band.name} | Venue: {concert.venue.title} | Date: {concert.date}")
 
 
-#Object Relationship Methods
+# Object Relationship Methods
 
-#Concerts
-#Concert band():should return the Band instance for this Concert
-# concert = session.query(Concert).first()#query the first concert
-# band_instance_for_concert = concert.band #get band instance for first concert 
-# print(f"Band name: {band_instance_for_concert.name}, Hometown: {band_instance_for_concert.hometown}")
+# Concerts
+# Concert band():should return the Band instance for this Concert
+concert = session.query(Concert).first()#query the first concert
+band_instance_for_concert = concert.band #get band instance for first concert 
+print(f"Band name: {band_instance_for_concert.name}, Hometown: {band_instance_for_concert.hometown}")
 
-# #Concert venue():should return the venue instance for this Concert
-# concert = session.query(Concert).first()#query the first concert
-# venue_instance_for_concert = concert.venue #get venue instance for first concert 
-# print(f"Venue title: {venue_instance_for_concert.title}, City: {venue_instance_for_concert.city}")
+#Concert venue():should return the venue instance for this Concert
+concert = session.query(Concert).first()#query the first concert
+venue_instance_for_concert = concert.venue #get venue instance for first concert 
+print(f"Venue title: {venue_instance_for_concert.title}, City: {venue_instance_for_concert.city}")
 
-# #Venues
-# #Venue concerts(): returns a collection of all the concerts for the Venue
-# venue = session.query(Venue).filter_by(title = "Sarova Whitesands Beach Resort").first()
-# venue_concerts = venue.concerts #get concerts for venue
-# print(f"concerts at {venue.title} in {venue.city}")#prints concert venue
-# for concert in venue_concerts:
-#     print(f"Concert Date: {concert.date}, Band: {concert.band.name}")#prints concerts in the concert table for the given venue
+#Venues
+#Venue concerts(): returns a collection of all the concerts for the Venue
+venue = session.query(Venue).filter_by(title = "Sarova Whitesands Beach Resort").first()
+venue_concerts = venue.concerts #get concerts for venue
+print(f"concerts at {venue.title} in {venue.city}")#prints concert venue
+for concert in venue_concerts:
+    print(f"Concert Date: {concert.date}, Band: {concert.band.name}")#prints concerts in the concert table for the given venue
 
-# #Venue bands(): returns a collection of all the bands performing at the Venue
-# venue = session.query(Venue).filter_by(title = "Sarova Whitesands Beach Resort").first()
-# venue_bands = venue.bands 
-# print(f"bands at {venue.title} in {venue.city}")
-# for band in venue_bands:
-#     print(f"Band Name: {band.name}, Hometown: {band.hometown}")
+#Venue bands(): returns a collection of all the bands performing at the Venue
+venue = session.query(Venue).filter_by(title = "Sarova Whitesands Beach Resort").first()
+venue_bands = venue.bands 
+print(f"bands at {venue.title} in {venue.city}")
+for band in venue_bands:
+    print(f"Band Name: {band.name}, Hometown: {band.hometown}")
 
-# #Bands
-# #band concerts():should return a collection of all the concerts that the Band has played
-# band = session.query(Band).first()#query the first band
-# band_concerts = band.concerts # get all concerts the band has played
-# print(f"Concerts played by {band.name}:")
-# for concert in band_concerts:
-#     print(f"Concert Date: {concert.date}, Venue: {concert.venue.title}")
+#Bands
+#band concerts():should return a collection of all the concerts that the Band has played
+band = session.query(Band).first()#query the first band
+band_concerts = band.concerts # get all concerts the band has played
+print(f"Concerts played by {band.name}:")
+for concert in band_concerts:
+    print(f"Concert Date: {concert.date}, Venue: {concert.venue.title}")
 
-# #band venues():should return a collection of all the venues that the Band has performed at
-# band = session.query(Band).first()#query the first band
-# band_venues = band.venues # get all concerts the band has played
-# print(f"Venues played by {band.name}:")
-# for venue in band_venues:
-#     print(f"Venue: {venue.title}, City: {venue.city}")
+#band venues():should return a collection of all the venues that the Band has performed at
+band = session.query(Band).first()#query the first band
+band_venues = band.venues # get all concerts the band has played
+print(f"Venues played by {band.name}:")
+for venue in band_venues:
+    print(f"Venue: {venue.title}, City: {venue.city}")
 
 
 #Aggregate and Relationship Methods
 
-#concert
-#Concert hometown_show(): returns true if the concert is in the band's hometown, false if it is not
-# concert = session.query(Concert).first() # Get the first concert
-# print(f"Band: {concert.band.name}, Hometown: {concert.band.hometown}")
-# print(f"Venue: {concert.venue.title}, City: {concert.venue.city}")
-# if concert.hometown_show():
-#     print(True)
-# else:
-#     print(False)
+# concert
+# Concert hometown_show(): returns true if the concert is in the band's hometown, false if it is not
+concert = session.query(Concert).first() # Get the first concert
+print(f"Band: {concert.band.name}, Hometown: {concert.band.hometown}")
+print(f"Venue: {concert.venue.title}, City: {concert.venue.city}")
+if concert.hometown_show():
+    print(True)
+else:
+    print(False)
 
 #Concert introduction(): returns a string with the band's introduction for this concert
-# concert = session.query(Concert).filter_by(date="2024/09/19").first()  # Get the concert, filter by date
-# print(concert.introduction())
+concert = session.query(Concert).filter_by(date="2024/09/19").first()  # Get the concert, filter by date
+print(concert.introduction())
 
-#band
+# band
 # Band play_in_venue(venue, date):takes a venue (Venue instance) and date (as a string) as arguments
-# creates a new concert for the band in that venue on that date\
-# band = session.query(Band).filter_by(name="Elani").first()  # Get the band "Elani"
-# venue = session.query(Venue).filter_by(title="Safari Park Hotel").first()  # Get the venue "Safari Park Hotel"
-# new_concert = band.play_in_venue(venue, "2024-10-30", session)# choose date for band to play
-# # Print the details of the new concert
-# print(f"New concert on {new_concert.date} at {new_concert.venue.title} by {new_concert.band.name}")
+# creates a new concert for the band in that venue on that date
+band = session.query(Band).filter_by(name="Elani").first()  # Get the band "Elani"
+venue = session.query(Venue).filter_by(title="Safari Park Hotel").first()  # Get the venue "Safari Park Hotel"
+new_concert = band.play_in_venue(venue, "2024-10-30", session)# choose date for band to play
+# Print the details of the new concert
+print(f"New concert on {new_concert.date} at {new_concert.venue.title} by {new_concert.band.name}")
 
 # all_introductions()Method:returns all introductions for the band's concerts
-# Get a band
-# band = session.query(Band).filter_by(name="Elani").first()  # Get the band "Elani"
-# introductions = band.all_introductions()# Get all introductions for this band
+#Get a band
+band = session.query(Band).filter_by(name="Elani").first()  # Get the band "Elani"
+introductions = band.all_introductions()# Get all introductions for this band
 
-# # Print each introduction
-# for intro in introductions:
-#     print(intro)
+# Print each introduction
+for intro in introductions:
+    print(intro)
 
 
-#Band most_performances() class method:returns the Band instance for the band that has played the most concerts
+# Band most_performances() class method:returns the Band instance for the band that has played the most concerts
 # Find the band with the most performances
-# band_with_most_concerts = Band.most_performances(session)
-# print(f"The band with the most performances is {band_with_most_concerts.name}.")
+band_with_most_concerts = Band.most_performances(session)
+print(f"The band with the most performances is {band_with_most_concerts.name}.")
 
 #def concert_on(date):returns the first concert on the given date at this venue
-# venue = session.query(Venue).filter_by(title="Sarova Whitesands Beach Resort").first()
-# concert = venue.concert_on(session, "2024/12/08")
+venue = session.query(Venue).filter_by(title="Sarova Whitesands Beach Resort").first()
+concert = venue.concert_on(session, "2024/12/08")
 
-# if concert:
-#     print(f"{concert.band.name} band performing at {concert.venue.title} on {concert.date}")
-# else:
-#     print("No concert found on that date.")
+if concert:
+    print(f"{concert.band.name} band performing at {concert.venue.title} on {concert.date}")
+else:
+    print("No concert found on that date.")
 
 #Most_frequent_band(): returns band with most concertas at a given venue
 venue = session.query(Venue).filter_by(title="Sarova Whitesands Beach Resort").first()
